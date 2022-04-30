b2641e5841a933dd3ea2f83779fa6be5e8f03245
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2022-04-30 21:08:19 +0100

    Fixed the alert popup to focus the input element when not active

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+// Declare all variables
+
 const input = document.querySelector('#input');
 const result = document.querySelector('.result');
 const resultCheck = document.querySelector('.resultCheck');
@@ -5,20 +7,29 @@ const showResult = document.querySelector('.showResult');
 const content = document.querySelector('.content');
 const btn = document.querySelector('.btn');
 
+
+// Call a focus method on the input element when the page loads
 window.addEventListener('load', () => {
   input.focus();
 });
 
+// Declare an Event listerner for a change in the input element which controls the result and functions later in the app
 input.addEventListener('change', (e) => {
   e.preventDefault();
 
   let myInput = input.value;
+
+  // Declare the random generated number and convert all to a number
   const myRandom = parseInt(Math.floor(Math.random() * 10));
 
+
+// Declare a checker for the condition when entered
   if (myInput.length === 0 && myInput === '' && myInput < 0) {
     resultCheck.textContent = '';
     showResult.textContent = '';
   }
+
+// Test and compare the input number to the random generated number
 
   if (input.value == myRandom) {
     showResult.textContent = `Your input number is ${myInput} and the random number generated is ${myRandom}`;
@@ -30,8 +41,10 @@ input.addEventListener('change', (e) => {
     resultCheck.textContent = 'You lose';
     content.style.background = '#ce0d0d';
     resultCheck.classList.add('lose');
-    // showResult.clear();
   }
+
+// Call a pop up 
+
   content.classList.add('active');
 
   console.log(myRandom);
@@ -39,8 +52,9 @@ input.addEventListener('change', (e) => {
   input.value = '';
   input.blur();
 
-  // showResult.clear();
 });
+
+// A click event when the button is clicked 
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -51,9 +65,14 @@ btn.addEventListener('click', (e) => {
   input.focus();
 });
 
+
+// This checks if a unwanted information is entered into the input element
 input.addEventListener('keyup', () => {
-  if (input.value > 9 || input.value <= -1) {
+  const regrex = /\S+@\S+\.\S=/;
+  if (input.value > 9 || input.value <= -1 || input.value == regrex) {
     input.value = '';
     alert('Input value must be between 0 and 9');
+    input.focus();
+    
   }
 });
